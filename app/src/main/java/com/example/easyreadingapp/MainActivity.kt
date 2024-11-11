eeee3ba4bd9049238596e4e3e954af2ccbe266b4
KevTav20 <kevin_tavera11@yahoo.com>
2024-11-11 15:46:34 -0600

    Create: Creacion de las pantallas basicas para una aplicacion de lectura digital, ademas de la estructura de base de proyecto

--- app/src/main/java/com/example/easyreadingapp/MainActivity.kt
+++ app/src/main/java/com/example/easyreadingapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.easyreadingapp.ui.theme.EasyReadingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EasyReadingAppTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EasyReadingAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/easyreadingapp/services/BookService.kt
@@ -0,0 +1,5 @@
+package com.example.easyreadingapp.services
+
+interface BookService {
+
+}
