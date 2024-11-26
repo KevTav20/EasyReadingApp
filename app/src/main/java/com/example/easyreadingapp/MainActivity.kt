8a7a419461bee2c1d7b3f4eacd79ab215eb232b8
Luis <luispmv07@hotmail.com>
2024-11-26 15:55:19 -0600

    Scaffold

--- app/src/main/java/com/example/easyreadingapp/MainActivity.kt
+++ app/src/main/java/com/example/easyreadingapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import coil3.compose.AsyncImage
+import com.example.easyreadingapp.ui.screens.ScaffolWrapper
 //import com.example.easyreadingapp.ui.screens.ImagenUrl
 import com.example.easyreadingapp.ui.theme.EasyReadingAppTheme
 
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EasyReadingAppTheme {
+                ScaffolWrapper()
             }
         }
     }
