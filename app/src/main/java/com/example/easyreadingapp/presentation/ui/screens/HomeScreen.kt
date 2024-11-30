79cf7763ee1151544aa0fb32f1a74a318d7e322b
Luis <luispmv07@hotmail.com>
2024-11-29 21:35:09 -0600

    Diseño Home Screen: Agregando el diseño y la funcionalidad de scroll en HomeScreen

--- app/src/main/java/com/example/easyreadingapp/presentation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/easyreadingapp/presentation/ui/screens/HomeScreen.kt
@@ -1,20 +1,27 @@
 package com.example.easyreadingapp.presentation.ui.screens
 
 import android.util.Log
+import android.widget.Space
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -39,23 +46,54 @@ import coil.request.ImageRequest
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.tooling.preview.Preview
+
 
 @Composable
-fun HomeScreen(innerPadding: PaddingValues, navController: NavController){
+fun BookImage(imageUrl:String){
     Column {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
-                //.placeholder(R.drawable.rick) // Asegúrate de tener este recurso en tu carpeta de drawables
-                .data("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmMQPyFbID_TEQ_nAlotYMPv-7Wm8JxQfAhQ&s") // URL hardcodeada
+                .data(imageUrl) // URL hardcodeada
                 .build(),
-            contentDescription = "Rick Sanchez",
+            contentDescription = "Libro",
             modifier = Modifier
-                .size(130.dp)
-                .clip(CircleShape)
-                .background(Color(0xFFD1D1D1))
-                .padding(10.dp)
-                .shadow(8.dp, CircleShape),
+                .width(145.dp)
+                .height(205.dp)
+                .padding(10.dp),
             contentScale = ContentScale.Crop
         )
     }
+}
+
+@Composable
+fun bookList(titulo:String, items:List<String>){
+    Column {
+        Text(text = titulo)
+        Spacer(modifier = Modifier.height(10.dp))
+        LazyRow {
+            items(items){ item ->
+                BookImage(imageUrl = item)
+            }
+        }
+    }
+}
+
+@Composable
+fun bookColumn(bookData: List<Pair<String, List<String>>>){
+    LazyColumn {
+        items(bookData) { (titulo, items) ->
+            bookList(titulo = titulo, items = items)
+        }
+    }
+}
+
+
+@Composable
+@Preview
+fun booklistView(){
+    var palabras = listOf<String>("Hola", "Amigos", "Como", "estan", "todos", "ustedes")
+    var titulo = "Soy un titulo de prueba"
+    bookList(titulo = titulo, items = palabras)
 }
