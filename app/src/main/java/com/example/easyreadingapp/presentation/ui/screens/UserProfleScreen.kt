9a4df71acfce3419f49d40502a5dc1e6986244c4
Alan Arana Carrillo <163362647+AlanArana23@users.noreply.github.com>
2024-12-03 23:06:24 -0600

    Cambios en el boton Cerrar Sesion a Cerrar App

--- app/src/main/java/com/example/easyreadingapp/presentation/ui/screens/UserProfleScreen.kt
+++ app/src/main/java/com/example/easyreadingapp/presentation/ui/screens/UserProfleScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.draw.clip
+import androidx.navigation.NavController
 import com.example.easyreadingapp.R
 
 @Composable
